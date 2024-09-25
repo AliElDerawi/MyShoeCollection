bdbdebb47cd15b62d5683a753d3799ff5bacbc74
Ali Al-Derawi <aalderawi2013@students.iugaza.edu.ps>
2024-09-25 14:56:30 +0300

    refactor: Use the (with) receiver function on ViewModel instance in BaseFragment to omit the need to write its name when using its variables

--- app/src/main/java/com/udacity/shoestore/data/BaseFragment.kt
+++ app/src/main/java/com/udacity/shoestore/data/BaseFragment.kt
@@ -1,14 +1,10 @@
 package com.udacity.shoestore.data
 
 import android.content.Context
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import com.google.android.material.snackbar.Snackbar
-import com.udacity.shoestore.utils.AppSharedMethods
 import com.udacity.shoestore.utils.AppSharedMethods.showToast
-import com.udacity.shoestore.utils.ShoeStoreApp
-import timber.log.Timber
 
 /**
  * Base Fragment to observe on the common LiveData objects
@@ -33,28 +29,32 @@ abstract class BaseFragment : Fragment() {
     override fun onStart() {
         super.onStart()
 
-        mViewModel.showErrorMessage.observe(viewLifecycleOwner) {
-            showToast(it)
-        }
-        mViewModel.showToast.observe(viewLifecycleOwner) {
-            showToast(it)
-        }
-        mViewModel.showSnackBar.observe(viewLifecycleOwner) {
-            Snackbar.make(this.requireView(), it, Snackbar.LENGTH_LONG).show()
-        }
-        mViewModel.showSnackBarInt.observe(viewLifecycleOwner) {
-            Snackbar.make(this.requireView(), mActivity.getString(it), Snackbar.LENGTH_LONG).show()
-        }
+        with(mViewModel) {
+            showErrorMessage.observe(viewLifecycleOwner) {
+                showToast(it)
+            }
+            showToast.observe(viewLifecycleOwner) {
+                showToast(it)
+            }
+            showSnackBar.observe(viewLifecycleOwner) {
+                Snackbar.make(this@BaseFragment.requireView(), it, Snackbar.LENGTH_LONG).show()
+            }
+            showSnackBarInt.observe(viewLifecycleOwner) {
+                Snackbar.make(
+                    this@BaseFragment.requireView(), mActivity.getString(it), Snackbar.LENGTH_LONG
+                ).show()
+            }
 
-        mViewModel.showToastInt.observe(viewLifecycleOwner) {
-            showToast(getString(it))
-        }
+            showToastInt.observe(viewLifecycleOwner) {
+                showToast(mActivity.getString(it))
+            }
 
-        mViewModel.showLoading.observe(viewLifecycleOwner) {
-            if (it) {
-                showWaiteDialog()
-            } else {
-                hideWaiteDialog()
+            showLoading.observe(viewLifecycleOwner) {
+                if (it) {
+                    showWaiteDialog()
+                } else {
+                    hideWaiteDialog()
+                }
             }
         }
 
