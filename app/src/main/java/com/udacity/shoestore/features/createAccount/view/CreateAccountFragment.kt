9379f75ad24c1bbd5a29be6a3e7698bb1f6d3452
Ali Al-Derawi <aalderawi2013@students.iugaza.edu.ps>
2024-09-25 14:28:28 +0300

    refactor: Using the new tech StateFlow for building form in CreateAccount Screen
    Changing the state of Create Account based on combining of multiple StateFlow values and conditions

--- app/src/main/java/com/udacity/shoestore/features/createAccount/view/CreateAccountFragment.kt
+++ app/src/main/java/com/udacity/shoestore/features/createAccount/view/CreateAccountFragment.kt
@@ -2,14 +2,20 @@ package com.udacity.shoestore.features.createAccount.view
 
 import android.app.Activity
 import android.content.Context
+import android.content.res.ColorStateList
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.edit
+import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
+import com.udacity.shoestore.R
 import com.udacity.shoestore.data.BaseFragment
 import com.udacity.shoestore.data.NavigationCommand
 import com.udacity.shoestore.databinding.FragmentCreateAccountBinding
@@ -17,6 +23,7 @@ import com.udacity.shoestore.features.createAccount.viewModel.CreateAccountViewM
 import com.udacity.shoestore.features.main.viewModel.MainViewModel
 import com.udacity.shoestore.utils.AppSharedData
 import com.udacity.shoestore.utils.AppSharedMethods.getSharedPreference
+import kotlinx.coroutines.launch
 import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -53,7 +60,7 @@ class CreateAccountFragment : BaseFragment() {
 
         mBinding = FragmentCreateAccountBinding.inflate(inflater, container, false)
         mSharedViewModel.setHideToolbar(true)
-        mLifecycleOwner = this
+        mLifecycleOwner = viewLifecycleOwner
         mBinding.lifecycleOwner = this
         mBinding.createAccountViewModel = mViewModel
         return mBinding.root
@@ -72,25 +79,70 @@ class CreateAccountFragment : BaseFragment() {
 
     private fun initViewModelObserver() {
 
-        mViewModel.completeCreateAccountLiveData.observe(mLifecycleOwner) { redirect ->
-            if (redirect) {
-                getSharedPreference().edit {
-                    putBoolean(AppSharedData.PREF_IS_LOGIN, true)
-                    putString(
-                        AppSharedData.PREF_USER_EMAIL,
-                        mBinding.emailTextInputEditText.text.toString()
-                    )
-                    putString(
-                        AppSharedData.PREF_USER_PASSWORD,
-                        mBinding.passwordTextInputEditText.text.toString()
+        with(mBinding){
+            mViewModel.completeCreateAccountLiveData.observe(mLifecycleOwner) { redirect ->
+                if (redirect) {
+                    getSharedPreference().edit {
+                        putBoolean(AppSharedData.PREF_IS_LOGIN, true)
+                        putString(
+                            AppSharedData.PREF_USER_EMAIL,
+                            emailTextInputEditText.text.toString()
+                        )
+                        putString(
+                            AppSharedData.PREF_USER_PASSWORD,
+                            passwordTextInputEditText.text.toString()
+                        )
+                    }
+
+                    mSharedViewModel.navigationCommand.value = NavigationCommand.To(
+                        CreateAccountFragmentDirections.actionCreateAccountFragmentToWelcomeFragment()
                     )
                 }
+            }
 
-                mSharedViewModel.navigationCommand.value = NavigationCommand.To(
-                    CreateAccountFragmentDirections.actionCreateAccountFragmentToWelcomeFragment()
-                )
+            lifecycleScope.launch {
+                lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                    mViewModel.isCreateAccountButtonEnabled.collect { isEnabled ->
+                        createAccountButton.backgroundTintList = if (isEnabled) {
+                            ColorStateList.valueOf(
+                                ResourcesCompat.getColor(
+                                    mActivity.resources, R.color.colorAccent, null
+                                )
+                            )
+                        } else {
+                            ColorStateList.valueOf(
+                                ResourcesCompat.getColor(
+                                    mActivity.resources, R.color.colorGrayF2, null
+                                )
+                            )
+                        }
+
+                        createAccountButton.strokeColor = if (isEnabled) {
+                            ColorStateList.valueOf(
+                                ResourcesCompat.getColor(
+                                    mActivity.resources, R.color.colorAccent, null
+                                )
+                            )
+                        } else {
+                            ColorStateList.valueOf(
+                                ResourcesCompat.getColor(
+                                    mActivity.resources, R.color.colorGray63, null
+                                )
+                            )
+                        }
+
+                        createAccountButton.setTextColor(
+                            if (isEnabled) {
+                                ResourcesCompat.getColor(mActivity.resources, R.color.colorWhite, null)
+                            } else {
+                                ResourcesCompat.getColor(mActivity.resources, R.color.colorBlack, null)
+                            }
+                        )
+                    }
+                }
             }
         }
+
     }
 
 }
--- app/src/main/java/com/udacity/shoestore/features/createAccount/viewModel/CreateAccountViewModel.kt
+++ app/src/main/java/com/udacity/shoestore/features/createAccount/viewModel/CreateAccountViewModel.kt
@@ -1,61 +1,66 @@
 package com.udacity.shoestore.features.createAccount.viewModel
 
-import android.app.Activity
 import android.app.Application
-import android.widget.EditText
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.udacity.shoestore.R
 import com.udacity.shoestore.data.BaseViewModel
-import com.udacity.shoestore.utils.AppSharedMethods.isEmpty
 import com.udacity.shoestore.utils.AppSharedMethods.isValidEmail
-import com.udacity.shoestore.utils.AppSharedMethods.showToast
 import com.udacity.shoestore.utils.SingleLiveEvent
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.stateIn
 
-class CreateAccountViewModel(val app: Application) : BaseViewModel(app)  {
+class CreateAccountViewModel(val app: Application) : BaseViewModel(app) {
 
     private val _completeCreateAccountLiveData = SingleLiveEvent<Boolean>()
     val completeCreateAccountLiveData: MutableLiveData<Boolean>
         get() = _completeCreateAccountLiveData
 
-    private val _emailLiveData = MutableStateFlow("")
-    val emailLiveData: StateFlow<String>
-        get() = _emailLiveData
+    private val _emailStateFlow = MutableStateFlow("")
+    val emailStateFlow: StateFlow<String>
+        get() = _emailStateFlow
 
-    private val _passwordLiveData = MutableStateFlow("")
-    val passwordLiveData: StateFlow<String>
-        get() = _passwordLiveData
+    private val _passwordStateFlow = MutableStateFlow("")
+    val passwordStateFlow: StateFlow<String>
+        get() = _passwordStateFlow
 
-    private val _confirmPasswordLiveData = MutableStateFlow("")
-    val confirmPasswordLiveData: StateFlow<String>
-        get() = _confirmPasswordLiveData
+    private val _confirmPasswordStateFlow = MutableStateFlow("")
+    val confirmPasswordStateFlow: StateFlow<String>
+        get() = _confirmPasswordStateFlow
+
+    val isCreateAccountButtonEnabled: StateFlow<Boolean> = combine(
+        _emailStateFlow, _passwordStateFlow, _confirmPasswordStateFlow
+    ) { email, password, confirmPassword ->
+        email.isNotEmpty() && email.isValidEmail() && password.isNotEmpty() && confirmPassword.isNotEmpty() && password == confirmPassword
+    }.stateIn(viewModelScope, SharingStarted.Lazily, false)
 
     fun onEmailTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
-        _emailLiveData.value = s.toString()
+        _emailStateFlow.value = s.toString()
     }
 
     fun onPasswordTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
-        _passwordLiveData.value = s.toString()
+        _passwordStateFlow.value = s.toString()
     }
 
     fun onConfirmPasswordTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
-        _confirmPasswordLiveData.value = s.toString()
+        _confirmPasswordStateFlow.value = s.toString()
     }
 
 
     fun createAccount(
     ) {
-        if (_emailLiveData.value.isEmpty()) {
+        if (_emailStateFlow.value.isEmpty()) {
             showToastInt.value = R.string.text_msg_please_enter_email
-        } else if (!_emailLiveData.value.isValidEmail()) {
+        } else if (!_emailStateFlow.value.isValidEmail()) {
             showToastInt.value = R.string.text_msg_enter_valid_email_address
-        } else if (_passwordLiveData.value.isEmpty()) {
+        } else if (_passwordStateFlow.value.isEmpty()) {
             showToastInt.value = R.string.text_msg_please_enter_password
-        } else if (_confirmPasswordLiveData.value.isEmpty()) {
+        } else if (_confirmPasswordStateFlow.value.isEmpty()) {
             showToastInt.value = R.string.text_msg_please_enter_confirm_password
-        } else if (_passwordLiveData.value != _confirmPasswordLiveData.value) {
+        } else if (_passwordStateFlow.value != _confirmPasswordStateFlow.value) {
             showToastInt.value = R.string.text_msg_password_mismatch
         } else {
             _completeCreateAccountLiveData.value = true
